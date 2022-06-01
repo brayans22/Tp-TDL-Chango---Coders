1930b03821e00c2f05a8b9df8a7ee576bff3c55a
Franco Bricchi <fbricchi@tdi.com.ar>
2022-06-01 19:18:03 -0300

    subo app modularizada

--- Tp-TDL-Chango-Coders/src/app/app.module.ts
+++ Tp-TDL-Chango-Coders/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    LoginComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,

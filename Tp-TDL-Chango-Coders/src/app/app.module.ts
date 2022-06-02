5920b64f28c6d006d60a6befcfc9fd18ccda3207
Franco Bricchi <fbricchi@tdi.com.ar>
2022-06-01 23:29:07 -0300

    subo cambios de routing

--- Tp-TDL-Chango-Coders/src/app/app.module.ts
+++ Tp-TDL-Chango-Coders/src/app/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { AppComponent } from './app.component'; 
+import { HeaderComponent } from './home/header/header.component'; 
+import { FooterComponent } from './home/footer/footer.component';
 import { LoginComponent } from './login/login.component';
-import { InicioComponent } from './inicio/inicio.component';
+import { InicioComponent } from './home/inicio/inicio.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { InicioComponent } from './inicio/inicio.component';
     HeaderComponent,
     FooterComponent,
     LoginComponent,
-    InicioComponent
+    InicioComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
